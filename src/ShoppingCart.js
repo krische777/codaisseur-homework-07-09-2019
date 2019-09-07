703704b0abd2a5db0a4603d244047e09295c8678
krische777 <kokobluewinkoko@gmail.com>
2019-09-07 17:00:06 +0200

    Completed Task 1 and Task 2

---
+++ src/ShoppingCart.js
@@ -0,0 +1,29 @@
+class ShoppingCart {
+    constructor(name) {
+        this.name=name
+        this.items=[]
+    }
+    getItems() {
+        return this.items
+    }
+    addItem(itemName, quantity, price) {
+        let item = {};
+        item.name=itemName;
+        item.quantity=quantity;
+        item.pricePerUnit=price;
+        this.items.push(item)
+
+    }
+    clear() {
+        this.items=[]
+    }
+    total() {
+        return this.items.reduce((sum, value) => {
+           return sum + value.pricePerUnit*value.quantity
+        }, 0)
+    }
+}
+ 
+
+
+module.exports=ShoppingCart
